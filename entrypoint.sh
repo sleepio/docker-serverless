d660a861c01a31a5ccd21e328313ed93d3b2ea78
Vladimir Kluev <vladimir.kluev@bighealth.com>
2020-08-31 16:49:40 -0700

    add comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,11 @@ if [[ ! $@ = *"--no-env"* ]] && [ $1 = "deploy" ]; then
     echo "deploy__whoami: ${deploy__whoami:-unknown}" >> ${ENV_FILE}
     echo "deploy__branch: ${deploy__branch:-unknown}" >> ${ENV_FILE}
     echo "deploy__HEAD: ${deploy__HEAD:-unknown}" >> ${ENV_FILE}
+    # NOTE (Vlad): This was added August 31st 2020 after setuptools suddenly stopped working
+    # due to the setuptools v50 upgrade. It's unclear at this time where the v50 of setup
+    # tools is coming from on lambda except that it is causing all newly deployed lambda functions to fail.
+    # setting this env variable prevents the distutils being overwritten by this code here:
+    # https://github.com/pypa/setuptools/blob/04e3df22df840c6bb244e9b27bc56750c44b7c85/_distutils_hack/__init__.py#L36
     echo "SETUPTOOLS_USE_DISTUTILS: ${SETUPTOOLS_USE_DISTUTILS:-stdlib}" >> ${ENV_FILE}
 
     echo "Merging .env.docker_serverless_build.yml and .env.project_generation.yml into .env.yml..."
